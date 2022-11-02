72847b0d0fface8bff2330ebc5461e882d922e6e
LorenzoSerina-UniBS <lorenzo.serina@unibs.it>
2022-11-02 18:24:08 +0100

    Lista utenti come datatable

--- app/user.py
+++ app/user.py
@@ -56,10 +56,8 @@ def user(id):
 
 @bp.route('/utenti', methods=('GET', 'POST'))
 @login_required
-def see_users():
-    users = User.query.filter(User.email != current_app.config["ADMIN_MAIL"]).all()
-    
-    return render_template('user/visualizza_utenti.html', users=users)
+def see_users():    
+    return render_template('user/visualizza_utenti.html')
 
 
 @bp.route('/promote/<user_id>', methods=('GET', 'POST'))
@@ -69,12 +67,9 @@ def promote(user_id):
     user = User.query.filter_by(id=user_id).first()
     user.superuser = True
     
-    db.session.add(user)
     db.session.commit()
-    
-    users = User.query.filter(User.email != current_app.config["ADMIN_MAIL"]).all()
-
-    return render_template('user/visualizza_utenti.html', title=current_app.config["LABELS"]["lista_utenti"], users=users)
+    return redirect(url_for('user.see_users'))
+    #return render_template('user/visualizza_utenti.html', title=current_app.config["LABELS"]["lista_utenti"])
 
 
 @bp.route('/demote/<user_id>', methods=('GET', 'POST'))
@@ -85,6 +80,76 @@ def demote(user_id):
     
     db.session.commit()
     
-    users=User.query.filter(User.email!=current_app.config["ADMIN_MAIL"]).all()
+    return redirect(url_for('user.see_users'))
 
-    return render_template('user/visualizza_utenti.html', title=current_app.config["LABELS"]["lista_utenti"], users=users)
+    #return render_template('user/visualizza_utenti.html', title=current_app.config["LABELS"]["lista_utenti"])
+
+@bp.route('/api/data')
+@login_required
+def data():
+    query = User.query.filter(User.email != current_app.config["ADMIN_MAIL"])
+
+    # search filter
+    search = request.args.get('search[value]')
+    if search:
+        query = query.filter(db.or_(
+            User.nome.like(f'%{search}%'),
+            User.cognome.like(f'%{search}%'),
+            User.nome_ufficio.like(f'%{search}%'),
+            User.ufficio.like(f'%{search}%'),
+            User.email.like(f'%{search}%')
+        ))
+    total_filtered = query.count()
+
+    # sorting
+    order = []
+    i = 0
+    while True:
+        col_index = request.args.get(f'order[{i}][column]')
+        if col_index is None:
+            break
+        col_code = request.args.get(f'columns[{col_index}][data]')
+        if col_code not in "code":
+            col_code = 'code'
+        descending = request.args.get(f'order[{i}][dir]') == 'desc'
+        col = getattr(User, col_code)
+        if descending:
+            col = col.desc()
+        order.append(col)
+        i += 1
+    if order:
+        query = query.order_by(*order)
+
+    # pagination
+    start = request.args.get('start', type=int)
+    length = request.args.get('length', type=int)
+    query = query.offset(start).limit(length)
+
+    
+    def render_file(user):
+        if user.superuser:
+            bottone = '<a  href="demote/' + str(user.id) + '" class="btn btn-danger btn-sm" role="button" aria-pressed="true">Revoca Privilegi</a>'
+        else:
+            bottone = '<a  href="promote/' + str(user.id) + '" class="btn btn-success btn-sm" role="button" aria-pressed="true">Assegna Privilegi</a>'
+            
+        return {
+            'nome_utente': user.nome + ' ' + user.cognome,
+            'email_utente': user.email,
+            'ufficio': user.ufficio,
+            'nome_ufficio': user.nome_ufficio,
+            'privilegi': bottone
+
+        }
+    distinct_users = {}
+    for user in query:
+        if user.id not in distinct_users:
+            distinct_users[user.id] = render_file(user)
+
+    # response
+    return {
+        'data': list(distinct_users.values()),
+        #'data': [render_file(file) for file in query],
+        'recordsFiltered': total_filtered,
+        'recordsTotal': User.query.count(),
+        'draw': request.args.get('draw', type=int),
+    }
